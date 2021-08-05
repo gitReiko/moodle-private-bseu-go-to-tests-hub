2c4c55858c11ac4d6e2c9fb4f76ae0142f7f5cc7
Denis Makouski (Reiko) <denis.makouski@bseu.by>
2021-08-05 12:00:20 +0300

    picture update

--- classes/main.php
+++ classes/main.php
@@ -72,7 +72,7 @@ class Main
     {
         $attr = array(
             'style' => 'width: 100%; max-width: 640px;',
-            'src' => '/blocks/gototestshub/pic/owl.jpg',
+            'src' => '/blocks/gototestshub/pic/hat.png',
             'alt' => get_string('click_on_image', 'block_gototestshub')
         );
 
