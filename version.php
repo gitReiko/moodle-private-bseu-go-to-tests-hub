d0f7027bfb9015a4b19f028a6beb4490bf0ade23
Denis <denis.makouski@bseu.by>
2022-07-06 10:11:34 +0300

    version++

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
 $plugin->component = 'block_gototestshub';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2021091502;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2022070600;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
